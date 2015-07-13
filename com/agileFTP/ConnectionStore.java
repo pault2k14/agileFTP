fc9f66f20ad442af999ab2689cb40364bc1a4ae0
johnschroeder <john.schroeder4@gmail.com>
2015-07-13 11:39:46 -0700

    Fixed fallthrough bug

--- com/agileFTP/ConnectionStore.java
+++ com/agileFTP/ConnectionStore.java
@@ -13,9 +13,11 @@ public class ConnectionStore {
     public void saveConnection(String [] input) {
         if(input.length == 5) {
             saveConnection(input[1], input[2], input[3], input[4]);
+            return;
         }
         if(input.length == 6) {
             saveConnection(input[1], input[2], input[3], input[4], input[5]);
+            return;
         }
         System.out.println("Incorrect number of parameters for save. Type 'help' for command syntax.");
     }

7e5cbe24ff13cad62347f810aebd093526a5cf0b
Paul Thompson <pbt@pdx.edu>
2015-07-26 17:31:51 -0700

    Refactored code, added checks in getDownloadPath to see if the download directory existed, if not create it. Someone else might want to do further refactoring and tests.

--- com/agileFTP/PathHelper.java
+++ com/agileFTP/PathHelper.java
@@ -1,16 +1,40 @@
 package com.agileFTP;
 
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.*;
+
 public class PathHelper {
 
     public static String delimiter = System.getProperty("file.separator");
 
-    public static String getDownloadsPath() {
+    public static String getDownloadsPath() throws IOException {
+
+        boolean success;
 
         String path = System.getProperty("user.home")
                 + delimiter
                 + "Downloads"
                 + delimiter;
+
+        File downloadPath = new File (path);
+
+        if (!downloadPath.exists()) {
+
+            success = downloadPath.mkdir();
+
+            if(success) {
+                System.out.println("Created local download directory.");
+            }
+
+            else {
+
+                IOException e = new IOException("Unable to create local download directory");
+                throw e;
+            }
+        }
 
         return path;
     }
